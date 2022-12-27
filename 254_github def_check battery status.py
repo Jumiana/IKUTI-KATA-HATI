7f95fdae24c11d6347ee78b7dd5020115cf1e4d8
chandradityaaaa <120441906+chandradityaaaa@users.noreply.github.com>
2022-12-27 13:24:21 +0700

    Add files via upload

---
+++ 254_github def_check battery status.py
@@ -0,0 +1,18 @@
+import psutil
+
+#Langkah awal install module psutil
+#https://pypi.org/project/psutil/#files
+#Selanjutnya module baru bisa di panggil
+
+def convertTime(seconds):
+    minutes, seconds=divmod(seconds, 60)
+    hours, minutes=divmod(minutes, 60)
+    return "%d:%02d:%02d"% (hours, minutes, seconds)
+
+battery=psutil.sensors_battery()
+percent=battery.percent
+time=convertTime(battery.secsleft)
+
+print ("Battery percentage: ",percent)
+print ("Power plugged in: ", battery.power_plugged)
+print ("Battery left: ",time)
